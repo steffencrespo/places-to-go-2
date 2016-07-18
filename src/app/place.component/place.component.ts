a9911a32aa7854758b7b93cda817df799bfd010e
Leonardo Steffen <leosteffen84@gmail.com>
2016-07-18 11:26:34 -0700

    adds css for place component, looks ugly as hEll but will get better when it ages

--- src/app/place.component/place.component.ts
+++ src/app/place.component/place.component.ts
@@ -3,9 +3,8 @@ import { Component, Input } from "@angular/core";
 @Component({
   moduleId: module.id,
   selector: 'place-component',
-  template: `<td class="place-name">{{place.name}}</td>
-      <td class="place-url">{{place.url}}</td>
-      <td class="place-rating">{{place.points}}</td>`
+  templateUrl: 'place.component.html',
+  styleUrls: ['place.component.css']
 })
 export class PlaceComponent {
   @Input() place;
